32fc0b9aae5f2edf41995f18fa3756d640d8c265
Thaw Tar Linn <40667690@live.napier.ac.uk>
2023-07-04 16:09:12 +0630

    7/4/23

--- static/ember.js
+++ static/ember.js
@@ -27,51 +27,40 @@ document.getElementById('search').onclick = ()=>{
       }
     })
 }
-//Adult
-document.getElementById('adult-minus').onclick = ()=>{
-  document.getElementById('adult').value = parseInt(document.getElementById('adult').value)-1;
+function increment(category) {
+  var input = document.getElementById(category);
+  var value = parseInt(input.value);
+  
+  if (value === 0) {
+    input.value = 1;
+  } else {
+    input.value = value + 1;
+  }
 }
-document.getElementById('adult-plus').onclick = ()=>{
-  document.getElementById('adult').value = parseInt(document.getElementById('adult').value)+1;
+function decrement(category) {
+  var input = document.getElementById(category);
+  var value = parseInt(input.value);
+  
+  if (value <= 1) {
+    alert("Quantity cannot be zero.");
+  } else {
+    input.value = value - 1;
+  }
 }
-//concession 
-document.getElementById('cc-minus').onclick = ()=>{
-  document.getElementById('cc').value = parseInt(document.getElementById('cc').value)-1;
-}
-document.getElementById('cc-plus').onclick = ()=>{
-  document.getElementById('cc').value = parseInt(document.getElementById('cc').value)+1;
-}
-
-//child
-document.getElementById('child-minus').onclick = ()=>{
-  document.getElementById('child').value = parseInt(document.getElementById('child').value)-1;
-}
-document.getElementById('child-plus').onclick = ()=>{
-  document.getElementById('child').value = parseInt(document.getElementById('child').value)+1;
-}
-
-//young child
-document.getElementById('young-minus').onclick = ()=>{
-  document.getElementById('young').value = parseInt(document.getElementById('young').value)-1;
-}
-document.getElementById('young-plus').onclick = ()=>{
-  document.getElementById('young').value = parseInt(document.getElementById('young').value)+1;
+function increment1(id) {
+  var input = document.getElementById(id);
+  var value = parseInt(input.value);
+  if (value < 2) {
+    input.value = value + 1;
+  }
 }
 
-//Bicycle
-document.getElementById('bi-minus').onclick = ()=>{
-  document.getElementById('bi').value = parseInt(document.getElementById('bi').value)-1;
-}
-document.getElementById('bi-plus').onclick = ()=>{
-  document.getElementById('bi').value = parseInt(document.getElementById('bi').value)+1;
-}
-
-//Wheelchairs
-document.getElementById('w-minus').onclick = ()=>{
-  document.getElementById('w').value = parseInt(document.getElementById('w').value)-1;
-}
-document.getElementById('w-plus').onclick = ()=>{
-  document.getElementById('w').value = parseInt(document.getElementById('w').value)+1;
+function decrement1(id) {
+  var input = document.getElementById(id);
+  var value = parseInt(input.value);
+  if (value > 0) {
+    input.value = value - 1;
+  }
 }
 function swapBoxes() {
   var from = document.getElementById("from");
