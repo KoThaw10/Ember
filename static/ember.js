3d7d0f27e54b030cb4392d3155d7c962ed3260e1
Thaw Tar Linn <40667690@live.napier.ac.uk>
2023-07-13 20:08:24 +0630

    last update!

--- static/ember.js
+++ static/ember.js
@@ -1,43 +1,82 @@
 fetch('/api/routes')
-  .then(r=>r.json())
-  .then(r=>{
-    for(let s of r.stops){
+  .then(r => r.json())
+  .then(r => {
+    for (let s of r.stops) {
       let opt = document.createElement('option');
       opt.innerHTML = s.name;
       opt.value = s.stopId;
       document.getElementById('from').append(opt);
     }
 
-    for(let s of r.stops){
+    for (let s of r.stops) {
       let opt = document.createElement('option');
       opt.innerHTML = s.name;
       opt.value = s.stopId;
       document.getElementById('to').append(opt);
     }
-})
-document.getElementById('search').onclick = ()=>{
+  })
+document.getElementById('search').onclick = () => {
   fetch(`/api/quote/${document.getElementById("from").value}/${document.getElementById("to").value}/${document.getElementById('whn').value}`)
-    .then(r=>r.json())
-    .then(r=>{
+    .then(r => r.json())
+    .then(r => {
       // var fromSelect = document.getElementById("from");
       // var toSelect = document.getElementById("to");
       // var fromText = fromSelect.options[fromSelect.selectedIndex].text;
       // var toText = toSelect.options[toSelect.selectedIndex].text;
 
       // document.getElementById('quotes').innerHTML = "From " + fromText + " to " + toText;
-      document.getElementById('quotes').innerHTML = `From ${document.getElementById("from").options[document.getElementById("from").selectedIndex].text} to ${document.getElementById("to").options[document.getElementById('to').selectedIndex].text}`;
-      for (let s of r.services){
+      document.getElementById('quotes').innerHTML = `<h4>From ${document.getElementById("from").options[document.getElementById("from").selectedIndex].text} to ${document.getElementById("to").options[document.getElementById('to').selectedIndex].text}</h4>`;
+      for (let s of r.services) {
+        var adultCount = parseInt(document.getElementById("adult").value);
+        var childCount = parseInt(document.getElementById("child").value);
+        var totalAmount = (adultCount * s.prices.adult) + (childCount * s.prices.child);
         let div = document.createElement('div');
-        div.innerHTML = `Departs: ${s.depart} Arrives:${s.arrive} Seats free:${s.availability.seat} Price:Adult:${s.prices.adult} Child:${s.prices.child} Concession:${s.prices.concession}`;
-        document.getElementById('quotes').append(div);
+        if (s.availability.seat > 0) {
+          const div = document.createElement("div");
+          div.innerHTML = `<b>Available</b> Adult: ${adultCount} Child: ${childCount}<br>
+                            Departs: ${s.depart} Arrives: ${s.arrive} Seats free: ${s.availability.seat}<br>
+                            Price: ${totalAmount}£-<br>
+                            <button class="book-button">Book</button>
+                            <div class="popup" style="display: none;">
+                              <div class="popup-content">
+                                <h2>Details of Booking</h2>
+                               <b> <center>From ${document.getElementById("from").options[document.getElementById("from").selectedIndex].text}
+                                to ${document.getElementById("to").options[document.getElementById('to').selectedIndex].text}</center></b><br>
+                                Departs: ${s.depart}<br>
+                                Arrives: ${s.arrive}<br>
+                                Adult: ${adultCount} Child: ${childCount}<br>
+                                Price: ${totalAmount}£-<br>
+                                <button class="close-button" type="submit" class="btn btn-primary">Cancel</button>
+                                <button class="confirm-button" type="submit" class="btn btn-primary">Confirm</button>
+                              </div>
+                            </div>`;
+
+          document.getElementById('quotes').appendChild(div);
+
+          document.getElementById('quotes').onclick=()=>{
+            if (event.target.classList.contains('book-button')) {
+              const popup = event.target.nextElementSibling;
+              popup.style.display = 'block';
+            } else if (event.target.classList.contains('close-button')) {
+              const popup = event.target.closest('.popup');
+              popup.style.display = 'none';
+            } else if (event.target.classList.contains('confirm-button')) {
+              alert('Thank you for booking!');
+            }
+          };
+        }
+        else {
+          div.innerHTML = `<b>unavaliable</b><br>Departs: ${s.depart} Arrives:${s.arrive} Seats free:${s.availability.seat}`;
+          document.getElementById('quotes-di').append(div);
+        }
       }
-      console.log(r);
+
     })
 }
 function increment(category) {
   var input = document.getElementById(category);
   var value = parseInt(input.value);
-  
+
   if (value === 0) {
     input.value = 1;
     updateDisplay();
@@ -49,7 +88,7 @@ function increment(category) {
 function decrement(category) {
   var input = document.getElementById(category);
   var value = parseInt(input.value);
-  
+
   if (value <= 1) {
     alert("Quantity cannot be zero.");
   } else {
@@ -61,7 +100,7 @@ function decrement(category) {
 function increment2(category) {
   var input = document.getElementById(category);
   var value = parseInt(input.value);
-  
+
   if (value === 0) {
     input.value = 1;
     updateDisplay();
@@ -73,7 +112,7 @@ function increment2(category) {
 function decrement2(category) {
   var input = document.getElementById(category);
   var value = parseInt(input.value);
-  
+
   if (value <= 1) {
     input.value = 0;
     updateDisplay();
@@ -91,6 +130,14 @@ function increment1(id) {
   }
 }
 
+function incrementW(id) {
+  var input = document.getElementById(id);
+  var value = parseInt(input.value);
+  if (value < 1) {
+    input.value = value + 1;
+    updateDisplay();
+  }
+}
 function decrement1(id) {
   var input = document.getElementById(id);
   var value = parseInt(input.value);
@@ -109,25 +156,7 @@ function swapBoxes() {
   from.value = to.value;
   to.value = tempValue;
 }
-
-function updateDisplay() {
-  var adultCount = parseInt(document.getElementById("adult").value);
-  var concessionCount = parseInt(document.getElementById("cc").value);
-  var childrenCount = parseInt(document.getElementById("child").value);
-  var youngChildrenCount = parseInt(document.getElementById("young").value);
-  var bicycleCount = parseInt(document.getElementById("bi").value);
-  var wheelchairCount = parseInt(document.getElementById("w").value);
-
-  var totalCount = adultCount + concessionCount + childrenCount + youngChildrenCount + bicycleCount + wheelchairCount;
-  var totalAmount = totalCount*20;
-
-  var displayDiv = document.getElementById("display");
-  displayDiv.innerHTML = "Totals: " + totalCount +"("+ totalAmount + "$)" 
-    "<br><br>Adults: " + adultCount +
-    "<br>Concession: " + concessionCount +
-    "<br>Children: " + childrenCount +
-    "<br>Young Children: " + youngChildrenCount +
-    "<br>Bicycles: " + bicycleCount +
-    "<br>Wheelchairs: " + wheelchairCount;
-    
-}
+var current_date = new Date();
+current_date.setDate(current_date.getDate() + 1);
+var format = current_date.toISOString().substr(0, 10);
+document.getElementById("whn").value = format;
